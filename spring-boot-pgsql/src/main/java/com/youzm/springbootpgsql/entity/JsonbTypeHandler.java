5aa6709e5251d95515a21e4da011e69ed5d06f46
user <youzhiming>
2022-04-08 10:26:16 +0800

    commit

---
+++ spring-boot-pgsql/src/main/java/com/youzm/springbootpgsql/entity/JsonbTypeHandler.java
@@ -0,0 +1,45 @@
+package com.youzm.springbootpgsql.entity;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.postgresql.util.PGobject;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author:youzhiming
+ * @date: 2022/2/15
+ * @description:
+ */
+public class JsonbTypeHandler extends BaseTypeHandler<Object> {
+    private static final PGobject jsonObject = new PGobject();
+
+    @Override
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, Object o, JdbcType jdbcType) throws SQLException {
+        if (preparedStatement != null) {
+            jsonObject.setType("jsonb");
+            jsonObject.setValue(JSON.toJSONString(o));
+            preparedStatement.setObject(i, jsonObject);
+        }
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return JSON.parse(resultSet.getString(s));
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return JSON.parse(resultSet.getString(i));
+    }
+
+    @Override
+    public Object getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return JSON.parse(callableStatement.getString(i));
+    }
+
+}
